b4717ba6257f24bc898a65467bf1b200dab094ea
TJMusiitwa <jonamusiitwa@outlook.com>
2020-10-07 21:04:44 +0100

    Initial Commit

---
+++ SwiftDine/SwiftDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDineApp.swift
+//  SwiftDine
+//
+//  Created by JTM on 07/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
